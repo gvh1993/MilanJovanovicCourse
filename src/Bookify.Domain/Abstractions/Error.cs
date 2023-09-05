046b497e3bf5cfed6f77b99550beea0c4133d014
Gert-Jan van Houwelingen <gertjanvanh@live.nl>
2023-09-05 23:28:08 +0200

    Add error and results

---
+++ src/Bookify.Domain/Abstractions/Error.cs
@@ -0,0 +1,7 @@
+namespace Bookify.Domain.Abstractions;
+
+public record Error(string Code, string Name)
+{
+    public static Error None = new(string.Empty, string.Empty);
+    public static Error NullValue = new("Error.NullValue", "Null value was provided");
+}
---
+++ src/Bookify.Domain/Abstractions/Result.cs
@@ -0,0 +1,59 @@
+using System;
+using System.Collections.Generic;
+using System.Diagnostics.CodeAnalysis;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Bookify.Domain.Abstractions;
+
+public class Result
+{
+    protected internal Result(bool isSuccess, Error error)
+    {
+        if (isSuccess && error != Error.None)
+        {
+            throw new InvalidOperationException();
+        }
+
+        if (!isSuccess && error == Error.None)
+        {
+            throw new InvalidOperationException();
+        }
+
+        IsSuccess = isSuccess;
+        Error = error;
+    }
+
+    public bool IsSuccess { get; }
+
+    public bool IsFailure => !IsSuccess;
+
+    public Error Error { get; }
+
+    public static Result Success() => new(true, Error.None);
+    public static Result Failure(Error error) => new(false, error);
+
+    public static Result<TValue> Success<TValue>(TValue value) => new(value, true, Error.None);
+
+    public static Result<TValue> Failure<TValue>(Error error) => new(default, false, error);
+
+    public static Result<TValue> Create<TValue>(TValue? value) => value is not null ? Success(value) : Failure<TValue>(Error.NullValue);
+}
+
+public class Result<TValue> : Result
+{
+    private readonly TValue? _value;
+
+    protected internal Result(TValue? value, bool isSuccess, Error error)
+        : base(isSuccess, error)
+    {
+        _value = value;
+    }
+
+    [NotNull]
+    public TValue Value => IsSuccess
+        ? _value!
+        : throw new InvalidOperationException("The value of a failure result can nmot be accessed");
+
+    public static implicit operator Result<TValue>(TValue value) => Create(value);
+}
